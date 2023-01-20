a7a0971b27b26331917c25c258f510b2bcc567ad
VedantPol <polvedant7@gmail.com>
2023-01-21 01:41:41 +0530

    did some stuff

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,7 @@ class _MyAppState extends State<MyApp> {
     return MaterialApp(
       theme: ThemeData(primarySwatch: Colors.red),
       debugShowCheckedModeBanner: false,
-      home: TabBarDemo(),
+      home: HomePage(),
     );
   }
 }
