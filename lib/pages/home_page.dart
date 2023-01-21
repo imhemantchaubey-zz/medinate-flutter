5867d9d917efc973ba7de0f168988e7771e1b002
imhemantchaubey <codewithhemant@gmail.com>
2023-01-21 08:29:39 +0530

    optimizations

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:dropdown_search/dropdown_search.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -9,28 +8,20 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  String dropdownvalue = 'Item 1';   
-  
-  // List of items in our dropdown menu
-  var items = [    
-    'Item 1',
-    'Item 2',
-    'Item 3',
-    'Item 4',
-    'Item 5',
-  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        centerTitle: true,
-        title: new Text("Medinate.com"),
-        leading:IconButton( 
-          icon:Image.asset('images/logo.jpeg', fit: BoxFit.contain,height: 32,),
-          onPressed: (){},
-        )
-      ),
-      
+          centerTitle: true,
+          title: const Text("Medinate.com"),
+          leading: IconButton(
+            icon: Image.asset(
+              'images/logo.jpeg',
+              fit: BoxFit.contain,
+              height: 32,
+            ),
+            onPressed: () {},
+          )),
       body: const Center(
         child: Text('Home Page'),
       ),
