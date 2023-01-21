361a88f44cb97a0a2e16f58cb93f3316d1e0550d
imhemantchaubey <codewithhemant@gmail.com>
2023-01-21 08:33:55 +0530

    added main carousel in home section

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -12,18 +13,64 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-          centerTitle: true,
           title: const Text("Medinate.com"),
           leading: IconButton(
             icon: Image.asset(
-              'images/logo.jpeg',
+              'images/medinate.png',
               fit: BoxFit.contain,
               height: 32,
             ),
             onPressed: () {},
           )),
-      body: const Center(
-        child: Text('Home Page'),
+      body: Container(
+        child: ListView(children: [
+          CarouselSlider(
+            items: [
+              Container(
+                margin: EdgeInsets.all(8.0),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10.0),
+                  image: DecorationImage(
+                    image: NetworkImage(
+                      "https://picsum.photos/200/400",
+                    ),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.all(8.0),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10.0),
+                  image: DecorationImage(
+                    image: NetworkImage("https://picsum.photos/200/400"),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.all(8.0),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10.0),
+                  image: DecorationImage(
+                    image: NetworkImage("https://picsum.photos/200/400"),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+            ],
+            options: CarouselOptions(
+              height: 380.0,
+              enlargeCenterPage: true,
+              autoPlay: true,
+              aspectRatio: 16 / 9,
+              autoPlayCurve: Curves.fastOutSlowIn,
+              enableInfiniteScroll: true,
+              autoPlayAnimationDuration: Duration(milliseconds: 800),
+              viewportFraction: 0.8,
+            ),
+          ),
+        ]),
       ),
     );
   }
