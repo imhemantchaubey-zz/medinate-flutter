3484c64a5ed041e97c827bc48995d8f12c6c556e
VedantPol <polvedant7@gmail.com>
2023-01-21 11:20:51 +0530

    linked pages

--- lib/main.dart
+++ lib/main.dart
@@ -24,7 +24,7 @@ class _MyAppState extends State<MyApp> {
     return MaterialApp(
       theme: ThemeData(primarySwatch: Colors.red),
       debugShowCheckedModeBanner: false,
-      home: TabBarDemo(),
+      home: HomePage(),
     );
   }
 }
